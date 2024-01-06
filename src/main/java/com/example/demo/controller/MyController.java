5b2b4615dd3c0d847f55d6dd1bc2710b1836f63b
MATHANKUMARR <rmathankumarcpm@gmail.com>
2024-01-06 23:39:02 +0530

    change#1

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
     
-    @GetMapping("")
+    @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to Software Development World Mathankumar R!";
     }

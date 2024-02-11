ab0623cb944abf58cf158db64e95bbf08aa25f62
MATHANKUMARR <rmathankumarcpm@gmail.com>
2024-02-11 20:18:24 +0530

    new ci/cd

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -11,3 +11,4 @@ public class MyController {
         return "Welcome to Software Development World Mathankumar R!";
     }
 }
+// docker run -p 8081:8081 -p 50000:500000 -d -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts
